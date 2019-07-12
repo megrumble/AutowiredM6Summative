46b9fd86ad667e346200798c7737b133e8fff4b2
Margaret Rumble <megrumble@gmail.com>
2019-07-12 11:59:59 -0400

    initial commit

---
+++ src/main/java/com/autowired/AutowiredM6SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.autowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiredM6SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowiredM6SummativeApplication.class, args);
+	}
+
+}
