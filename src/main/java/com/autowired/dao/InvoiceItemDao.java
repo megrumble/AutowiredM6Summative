c0847c0f34d61ac9314c3b0c1f3b2634fd7e3092
AJTalford <ajtalford@gmail.com>
2019-07-14 14:39:00 -0400

    Initial viewmodel

--- src/main/java/com/autowired/dao/InvoiceItemDao.java
+++ src/main/java/com/autowired/dao/InvoiceItemDao.java
@@ -1,5 +1,7 @@
 package com.autowired.dao;
 
+import com.autowired.model.InvoiceItem;
+
 import java.util.List;
 
 public interface InvoiceItemDao {
@@ -12,7 +14,7 @@ public interface InvoiceItemDao {
 
     /**
      * Retrieves a InvoiceItem object from the database using a given InvoiceItem id
-     * @param InvoiceItemId
+     * @param invoiceItemId
      * @return InvoiceItem - The InvoiceItem object is returned to the RestController
      */
     InvoiceItem getInvoiceItem(int invoiceItemId);
@@ -25,13 +27,13 @@ public interface InvoiceItemDao {
 
     /**
      * Updates an InvoiceItem from the InvoiceItem object sent from the RestController
-     * @param InvoiceItem - The InvoiceItem object sent from the RestController
+     * @param invoiceItem - The InvoiceItem object sent from the RestController
      */
     void updateInvoiceItem(InvoiceItem invoiceItem);
 
     /**
      * Deletes a InvoiceItem from the database using a InvoiceItemID from the RestController
-     * @param InvoiceItemId
+     * @param invoiceItemId
      */
     void deleteInvoiceItem(int invoiceItemId);
 }
